da93ac22a7d1437d1dc0bb9a7a8fdfbf8e67f1c5
antoniopereirarebecca-sudo <antonio.pereira.rebecca@escola.pr.gov.br>
2026-08-31 12:39:30 -0300

    ddd

--- iscript.js
+++ iscript.js
@@ -20,7 +20,36 @@ function prepararReacoes() {
         const botaoDOIS = botoes[1];
 
         const contadorBotaoUM = botaoUM.querySelector("span");
-        
+        const contadorBotaoDOIS = botaoDOIS.querySelector("span");
 
-        })
+         const idCard = `card-${indice + 1}`;
+
+         const chavebotaoUM = `${idCard}-botaoUM`;
+         const chavebotaoDOIS = `${idCard}-botaoDOIS`;
+
+        let UM = Number(localStorage.getItem(chavebotaoUM)) ||0;
+        let DOIS = Number(localStorage.getItem(chavebotaoDOIS)) ||0;
+        
+        contadorBotaoUM.textContent = UM;
+        contadorBotaoDOIS.textContent = DOIS;
+
+        botaoUM.addEventListener("click", () =>{
+            UM++;
+            contadorBotaoUM.textContent = UM;
+
+            localStorage.setItem(
+                chavebotaoUM,UM
+            );
+        });
+        botaoDOIS.addEventListener("click", () =>{
+            DOIS++;
+            contadorBotaoDOIS.textContent = DOIS;
+
+            localStorage.setItem(
+                chavebotaoDOIS,DOIS
+            );
+        });
+    })
+
+
 }
